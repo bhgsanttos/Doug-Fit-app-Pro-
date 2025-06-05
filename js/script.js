57e86c007af4c45db284bef1e9d0f78432246e66
bhgsanttos <68077822+bhgsanttos@users.noreply.github.com>
2025-06-05 00:59:24 -0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function showTab(tabName) {
+  const tabs = document.querySelectorAll('.tab-content');
+  tabs.forEach(tab => tab.style.display = 'none');
+  document.getElementById(tabName).style.display = 'block';
+}
+
+const themeToggle = document.getElementById("themeToggle");
+themeToggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+  themeToggle.textContent = document.body.classList.contains("dark-mode") ? "☀️" : "🌙";
+});
